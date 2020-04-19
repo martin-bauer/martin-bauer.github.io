9131892ac81e2a7dc8e5faa75c7e433f66029fbb
Martin Bauer <mba@adorsys.de>
2020-04-19 20:01:41 +0200

    Page refactor(Responsiveness,Hover-Effects,cleanup)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,22 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "00b1bf420a5828e1d041c0d3c37ac7cd",
-"/": "00b1bf420a5828e1d041c0d3c37ac7cd",
-"main.dart.js": "a67aa74a84bafc36b48a65139267f45e",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+  "index.html": "dc49be1f1f90f63a9a811fbb6b2d927c",
+"/": "dc49be1f1f90f63a9a811fbb6b2d927c",
+"main.dart.js": "36b8fe9fad1b0c78766cbda2d5036c3d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "771cda94fb9701f723c73c071fe9f0e7",
-"assets/LICENSE": "68f14b63437262f2d906095a5cdbad25",
-"assets/imgs/psxme.png": "7dcb824ef46e04cbd8b5a77bc8e0bbd4",
-"assets/imgs/psxme2.png": "677a5b710cb71e0e873d07250ea5d4ff",
-"assets/imgs/psx.jpg": "3e15ce67327007d64fc76614d7f3c06f",
-"assets/imgs/psx.png": "5081f234906e019481af7b5fe34016e1",
-"assets/AssetManifest.json": "e1ecc0e4c7c6529e128792c8e31b02bf",
+"assets/LICENSE": "dac72842292e7e61fad8155e2139cedc",
+"assets/imgs/psxme2.png": "ccd3662cc7be1e812abefc270be03974",
+"assets/imgs/psxx.png": "98e46a2b4eefbd9300e5b7405286716c",
+"assets/AssetManifest.json": "aac4c6ff0cd5f61857140280b1b44641",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"favicon2-min.png": "88e699401c858fe6ce12feb0702b7a3a"
 };
 
 self.addEventListener('activate', function (event) {
