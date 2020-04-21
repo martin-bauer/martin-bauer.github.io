e2966c86f14be699d4909fe717d6741a0be767a8
Martin Bauer <mba@adorsys.de>
2020-04-21 21:47:00 +0200

    Add firestore and analytics, change font and remove dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:bama1033pages/services/calls_and_messages_service.dart';
 import 'package:bama1033pages/services/service_locator.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'dart:html' as html;
 import 'package:bama1033pages/extensions/hover_extensions.dart';
+import 'dart:html' as html;
 
 void main() {
   setupLocator();
@@ -17,6 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Martinez`s Page',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
+        fontFamily: 'Ubuntu',
       ),
       home: MyHomePage(title: 'Martin Bauers Page'),
     );
@@ -65,122 +65,129 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
 //        resizeToAvoidBottomInset: false,
         body: Stack(
+      children: <Widget>[
+        new Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('imgs/psxx.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            new Container(
+            Container(
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('imgs/psxx.jpg'),
-                  fit: BoxFit.cover,
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromRGBO(40, 40, 45, 0.01),
+                    Colors.black87,
+                  ],
+                ),
+              ),
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    new Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: marginVerticalAvatar),
+                      decoration: myBoxDecoration(),
+                      child: CircleAvatar(
+                          radius: 100,
+                          backgroundImage: AssetImage('imgs/psxme2.png')),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: 'Hey, ich bin Martin\n '
+                            'und ich mache was ich will',
+                        style: TextStyle(
+                          fontSize: fontSize,
+                          color: Colors.white,
+                          fontFamily: 'Ubuntu',
+                        ),
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: marginIconsHorizontal),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: marginIconsHorizontal,
+                                vertical: marginIconsVertical),
+                            child: IconButton(
+                              icon: Icon(Icons.code),
+                              color: Colors.white70,
+                              iconSize: iconsize + 10,
+                              onPressed: () {
+                                html.window.open('https://github.com/bama1033',
+                                    'Github/bama1033');
+                              },
+                            ).showCursorOnHover.elevateUpOnHover,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: marginIconsHorizontal,
+                                vertical: marginIconsVertical),
+                            child: IconButton(
+                              icon: Icon(Icons.mail),
+                              iconSize: iconsize,
+                              color: Colors.white70,
+                              onPressed: () {
+                                _service.sendEmail();
+                              },
+                            ).showCursorOnHover.elevateUpOnHover,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: marginIconsHorizontal,
+                                vertical: marginIconsVertical),
+                            child: Icon(
+                              Icons.favorite,
+                              color: Colors.white70,
+                              size: iconsize,
+                            ).elevateUpOnHover,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: marginIconsHorizontal,
+                                vertical: marginIconsVertical),
+                            child: Icon(
+                              Icons.build,
+                              color: Colors.white70,
+                              size: iconsize,
+                            ).elevateUpOnHover,
+                          ),
+                        ],
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        html.window
+                            .open('https://flutter.dev/web', 'flutter.dev/web');
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          'Made by me with Flutter',
+                          style: TextStyle(fontSize: 10, color: Colors.white70),
+                        ),
+                      ),
+                    ).showCursorOnHover.elevateUpOnHover,
+                  ],
                 ),
               ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Color.fromRGBO(40, 40, 45, 0.01),
-                        Colors.black87,
-                      ],
-                    ),
-                  ),
-                  child: Center(
-                    child: Column(
-                      children: <Widget>[
-                        new Container(
-                          margin:
-                              EdgeInsets.symmetric(vertical: marginVerticalAvatar),
-                          decoration: myBoxDecoration(),
-                          child: CircleAvatar(
-                              radius: 100,
-                              backgroundImage: AssetImage('imgs/psxme2.png')),
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text: 'Hey, ich  bin Martin\n '
-                                'und ich mache was ich will',
-                            style: GoogleFonts.lato(
-                              fontSize: fontSize,
-                              color: Colors.white,
-                            ),
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: marginIconsHorizontal),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: marginIconsHorizontal, vertical:marginIconsVertical),
-
-                                child: IconButton(
-                                  icon: Icon(Icons.code),
-                                  color: Colors.white70,
-                                  iconSize: iconsize + 10,
-                                  onPressed: () {
-                                    html.window.open(
-                                        'https://github.com/bama1033',
-                                        'Github/bama1033');
-                                  },
-                                ).showCursorOnHover.elevateUpOnHover,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: marginIconsHorizontal, vertical:marginIconsVertical),
-                                child: IconButton(
-                                  icon: Icon(Icons.mail),
-                                  iconSize: iconsize,
-                                  color: Colors.white70,
-                                  onPressed: () {
-                                    _service.sendEmail();
-                                  },
-                                ).showCursorOnHover.elevateUpOnHover,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: marginIconsHorizontal, vertical:marginIconsVertical),
-                                child: Icon(
-                                  Icons.favorite,
-                                  color: Colors.white70,
-                                  size: iconsize,
-                                ).elevateUpOnHover,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: marginIconsHorizontal, vertical:marginIconsVertical),
-                                child: Icon(
-                                  Icons.build,
-                                  color: Colors.white70,
-                                  size: iconsize,
-                                ).elevateUpOnHover,
-                              ),
-                            ],
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () {
-                            html.window.open(
-                                'https://flutter.dev/web', 'flutter.dev/web');
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(12.0),
-                            child: Text(
-                              'Made by me with Flutter',
-                              style: GoogleFonts.lato(
-                                  fontSize: 10, color: Colors.white70),
-                            ),
-                          ),
-                        ).showCursorOnHover.elevateUpOnHover,
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
           ],
-        ));
+        ),
+      ],
+    ));
   }
 }
