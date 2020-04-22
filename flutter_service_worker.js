c6f9fc2ca2ca16f665f7df7218499c93287b7807
Martin Bauer <mba@adorsys.de>
2020-04-22 18:51:53 +0200

    Fix small screensizes again, Add cute little favorite function

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "28a84781ab92206382bd03c0e33df2ce",
 "/": "28a84781ab92206382bd03c0e33df2ce",
-"main.dart.js": "1e6c46e0bedbba6fecfb4babfbe134b8",
+"main.dart.js": "c8ede9019dc7c7fa3192e523b3f7ec2c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "771cda94fb9701f723c73c071fe9f0e7",
